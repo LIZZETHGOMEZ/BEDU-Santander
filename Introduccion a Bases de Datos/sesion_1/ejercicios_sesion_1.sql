42b27fc66336df7db21d2b24ad6b1e3dadbdb53d
LIZZETH GÓMEZ RODRÍGUEZ <lizzethgomezrodriguez@ciencias.unam.mx>
2021-05-23 20:32:54 -0500

    Update ejercicios_sesion_1.sql

--- Introduccion a Bases de Datos/sesion_1/ejercicios_sesion_1.sql
+++ Introduccion a Bases de Datos/sesion_1/ejercicios_sesion_1.sql
@@ -46,12 +46,14 @@ WHERE jobTitle ='Sales Rep' OR officeCode = 1;
 -- 8. Dentro de la tabla employees, obtén el apellido, nombre y código de oficina de todos los empleados 
 -- que tenga código de oficina 1, 2 o 3.
 SELECT lastName, firstName, jobTitle, officeCode
-FROM employees WHERE officeCode IN (1,2,3);
+FROM employees
+WHERE officeCode IN (1,2,3);
 
 -- 9. Dentro de la tabla employees, obten el apellido, nombre y puesto de todos los empleados que 
 -- tengan un puesto distinto a Sales Rep.
 SELECT lastName, firstName, jobTitle
-FROM employees WHERE jobTitle != 'Sales Rep';
+FROM employees
+WHERE jobTitle != 'Sales Rep';
 
 -- 10. Dentro de la tabla employees, obtén el apellido, nombre y código de oficina de todos los empleados 
 -- cuyo código de oficina sea mayor a 5.
@@ -61,7 +63,9 @@ WHERE officeCode > 5;
 
 -- 11. Dentro de la tabla employees, obtén el apellido, nombre y código de oficina de todos los empleados 
 -- cuyo cdigo de oficina sea menor o igual 4.
-SELECT lastName, firstName, officeCode FROM employees WHERE officeCode <= 4;
+SELECT lastName, firstName, officeCode 
+FROM employees
+WHERE officeCode <= 4;
 
 -- 12. Dentro de la tabla customers, obtén el nombre, país y estado de todos los clientes cuyo país 
 -- sea USA y cuyo estado sea CA.
@@ -79,7 +83,7 @@ WHERE country = 'USA' AND state = 'CA' AND creditLimit > 10000;
 -- 14. Dentro de la tabla customers, obtén el nombre y país de todos los clientes cuyo país sea USA o France
 SELECT customerName, country 
 FROM customers 
-WHERE country = 'USA'OR country = 'France';
+WHERE country = 'USA' OR country = 'France';
 
 -- 15. Dentro de la tabla customers, obtén el nombre, pas y límite de crédito de todos los clientes 
 -- cuyo país sea USA o France y cuyo límite de crédito sea mayor a 100000. Para este ejercicio ten cuidado con los paréntesis.
@@ -89,30 +93,38 @@ WHERE country = 'USA' OR country = 'France' AND creditLimit > 10000;
 
 -- 16. Dentro de la tabla offices, obtén el código de la oficina, ciudad, teléfono y país de aquellas oficinas que se encuentren en USA o France.
 SELECT* FROM offices;
-SELECT postalCode, city, phone, country FROM offices WHERE country = 'USA' OR country = 'France';
+SELECT postalCode, city, phone, country
+FROM offices
+WHERE country = 'USA' OR country = 'France';
 
 -- 17. Dentro de la tabla offices, obtén el código de la oficina, ciudad, teléfono y país de aquellas oficinas que no se encuentren en USA o France.
-SELECT postalCode, city, phone, country FROM offices WHERE country != 'USA' AND country != 'France';
+SELECT postalCode, city, phone, country
+FROM offices
+WHERE country != 'USA' AND country != 'France';
 
 -- 18. Dentro de la tabla orders, obtén el número de orden, número de cliente, estado y 
 -- fecha de envío de todas las órdenes con el número 10165, 10287 o 10310.
 SELECT* FROM orders;
-SELECT orderNumber, customerNumber,`status`, shippedDate FROM orders 
+SELECT orderNumber, customerNumber,`status`, shippedDate 
+FROM orders 
 WHERE orderNumber IN (10165, 10287, 10310);
 
 -- 19. Dentro de la tabla customers, obtén el apellido de contacto y nombre de cada cliente y ordena los resultados por apellido de forma ascendente.
 SELECT* FROM customers;
-SELECT contactLastName, customerName FROM customers
+SELECT contactLastName, customerName
+FROM customers
 ORDER BY contactLastName;
 
 -- 20. Dentro de la tabla customers, obtén el apellido de contacto y nombre de cada cliente y 
 -- ordena los resultados por apellido de forma descendente.
-SELECT contactLastName, customerName FROM customers
+SELECT contactLastName, customerName
+FROM customers
 ORDER BY contactLastName DESC;
 
 -- 21. Dentro de la tabla customers, obtén el apellido y nombre de cada cliente y ordena los resultados por apellido de forma descendente 
 -- y luego por nombre de forma ascendente.
-SELECT contactLastName, customerName FROM customers
+SELECT contactLastName, customerName
+FROM customers
 ORDER BY contactLastName DESC, customerName;
 
 -- 22. Dentro de la tabla customers, obtén el número de cliente, nombre de cliente 
